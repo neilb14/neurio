f3ee73c4f1b68a61f07ca690984aa7a38de975b6
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-03-13 19:37:29 -0600

    added steps

--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 import sys,requests
 
+steps = [0,140,250,500,700,1000,2000]
+
 if(len(sys.argv) < 2):
     raise IncorrectArgumentsException()
 
 ip_address = sys.argv[1]
 url = "http://{}/current-sample".format(ip_address)
+
+def find_step(power):
+    for i, value in enumerate(steps):
+        if(value >= power):
+            return i
 
 response = requests.get(url)
 if(response.status_code != 200):
@@ -14,5 +21,6 @@ if(response.status_code != 200):
 data = response.json()
 for channel in data["channels"]:
     if(channel["type"] == "CONSUMPTION"):
-        print(data["timestamp"], " ", channel["p_W"],"W")
+        power = float(channel["p_W"])
+        print("[{}] {}W ({})".format(data["timestamp"], power,find_step(power)))
 
