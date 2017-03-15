4db8a32c03b2436840543ab9c258d14afb09e6a3
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-03-14 22:17:53 -0600

    cleaning up led cycler

--- cycle_led.py
+++ cycle_led.py
@@ -1,58 +1,23 @@
-import RPi.GPIO as GPIO
 import time
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+from led import pi
 
 # Red   Vf= 2.0 If= 20mA    R= 65ohm using~68ohm | R=150ohm @5V supply
 # Green Vf= 3.2 If= 20mA    R= 5ohm             | R=90                                                              ohm @5V supply
 # Blue  Vf= 3.2 If= 20mA    R= 5ohm             | R=90ohm @5V supply
 
-sequence = [
-    [0,0,0],    #0 0 0 white
-    [0,0,1],    #0 0 1 yellow
-    [1,0,1],    #1 0 1 green
-    [1,0,0],    #1 0 0 light blue
-    [1,1,0],    #1 1 0 dark blue
-    [0,1,0],    #0 1 0 violet
-    [0,1,1]    #0 1 1 red    
-]
-
-ledRed = 18
-ledGreen = 17
-ledBlue = 22
-GPIO.setup(ledRed, GPIO.OUT)
-GPIO.setup(ledGreen, GPIO.OUT)
-GPIO.setup(ledBlue, GPIO.OUT)
-
-GPIO.output(ledRed, 1)
-GPIO.output(ledGreen,1)
-GPIO.output(ledBlue,1)
-
-for s in sequence:    
-    red = s[0]
-    green = s[1]
-    blue = s[2]
-    print(red, green, blue)
-    GPIO.output(ledRed,red)
-    GPIO.output(ledGreen,green)
-    GPIO.output(ledBlue,blue)
+sequence = ["white", "yellow", "green", "turquoise", "blue", "violet", "red"]
+pi.show("off")
+for colour in sequence:    
+    pi.show(colour)
     time.sleep(2)
 
 for i in range(0,10):
-    GPIO.output(ledRed,0)
-    GPIO.output(ledGreen,0)
-    GPIO.output(ledBlue,0)
-    time.sleep(0.2)
-    GPIO.output(ledRed,0)
-    GPIO.output(ledGreen,1)
-    GPIO.output(ledBlue,1)
+    pi.show("white")
+    time.sleep(0.3)
+    pi.show("red")
     time.sleep(0.3)
 
-GPIO.output(ledRed, 1)
-GPIO.output(ledGreen,1)
-GPIO.output(ledBlue,1)
-
+pi.show("off")
 GPIO.cleanup()
 print('Done Fancy LED')
 
